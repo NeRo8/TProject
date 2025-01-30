1ccb005a1a8d45c93e24865947c15bab9f26a19f
Nero <artur.duchenko@gmail.com>
2025-01-30 14:02:52 +0000

    Edit App.test.tsx

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -6,7 +6,7 @@ import React from 'react';
 import {render, screen} from '@testing-library/react-native';
 import App from '../App';
 
-test('renders correctly', async () => {
+test('renders app file correctly', async () => {
   render(<App />);
   expect(screen.getByText('Read the docs to discover what to do next:')).toBeTruthy();
 });
