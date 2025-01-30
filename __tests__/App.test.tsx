f2f7c1bfb74e790d05424d1ba3e29c4d19f64700
Nero <artur.duchenko@gmail.com>
2025-01-30 17:02:49 +0200

    fix: modify app file, change test

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -6,7 +6,10 @@ import React from 'react';
 import {render, screen} from '@testing-library/react-native';
 import App from '../App';
 
-test('renders app file correctly', async () => {
-  render(<App />);
-  expect(screen.getByText('Read the docs to discover what to do next:')).toBeTruthy();
+
+describe('App file', () => {
+  test('render app file', async () => {
+    render(<App />);
+    expect(screen.getByText('App screen')).toBeTruthy();
+  });
 });
