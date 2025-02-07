3c20a15702e2a0b8296ba025cf31644fd77c11e7
Artur Duchenko <artur.duchenko@gmail.com>
2025-02-07 17:39:35 +0200

    fix: modify dummy test description

--- Dummy.test.tsx
+++ Dummy.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react-native';
 import Dummy from './Dummy';
 
-describe('App file', () => {
-  test('render app file', async () => {
+describe('Dummy file', () => {
+  test('render dummy file', async () => {
     render(<Dummy />);
     expect(screen.getByText('Dummy text')).toBeTruthy();
   });
